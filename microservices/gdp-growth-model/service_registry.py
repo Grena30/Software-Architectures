6b62ca566105cbc452117490a4f3273d6fda0fa1
dgutu <dgutu@griddynamics.com>
2025-11-13 18:27:36 +0200

    fix: update container launch method

--- microservices/gdp-growth-model/service_registry.py
+++ microservices/gdp-growth-model/service_registry.py
@@ -13,23 +13,23 @@ GDP_MODEL_NAME = os.getenv("GDP_MODEL_NAME", "LinearRegression")
 
 def register_gdp_model():
     consul = Consul(host=HOST, port=SERVICE_DISCOVERY_PORT)
-    ip_address = socket.gethostbyname(socket.gethostname())
-    service_id = f"gdp-growth-model-{GDP_MODEL_NAME}-{ip_address}"
+    container_hostname = socket.gethostname()
+    service_id = f"gdp-growth-model-{GDP_MODEL_NAME}-{container_hostname}"
 
     consul.agent.service.register(
         name="gdp-growth-model",
         service_id=service_id,
-        address=ip_address,
+        address=container_hostname,
         port=SERVICE_PORT,
         tags=["model", "gdp", f"model:{GDP_MODEL_NAME}"],
-        check={"http": f"http://{ip_address}:{SERVICE_PORT}/health", "interval": "10s"},
+        check={"http": f"http://{container_hostname}:{SERVICE_PORT}/health", "interval": "10s"},
     )
     print(f"Registered {service_id} with model {GDP_MODEL_NAME}")
 
 
 def deregister_gdp_model():
     consul = Consul(host=HOST, port=SERVICE_DISCOVERY_PORT)
-    ip_address = socket.gethostbyname(socket.gethostname())
-    service_id = f"gdp-growth-model-{GDP_MODEL_NAME}-{ip_address}"
+    container_hostname = socket.gethostname()
+    service_id = f"gdp-growth-model-{GDP_MODEL_NAME}-{container_hostname}"
     consul.agent.service.deregister(service_id)
     print(f"Deregistered {service_id} from Consul")
